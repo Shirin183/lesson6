b5e34549b05199feaddf65377e969d62c6c5aecf
Shirin183 <rumayarumiya@gmail.com>
2025-04-14 22:06:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let numbers = [10, 20, 30, 40];
+let names = ['Alice', 'Bob', 'Charlie'];
+
+console.log(numbers)
+console.log(numbers[1])
+
+console.log(numbers.length)
+
+console.log(names)
+
+names.push('Jorabekxonim')
+console.log(names)
+
+names.pop()
+console.log(names)
+
+
+
+  
+
